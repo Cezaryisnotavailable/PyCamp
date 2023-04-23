00e824ae91ebf12a7f73e5bf3f9809d57848aff8
Cezary Cezary <cezary.game@gmail.com>
2023-04-23 22:25:42 +0200

    class Deck added

--- Module_1/blackjack.py
+++ Module_1/blackjack.py
@@ -1,5 +1,26 @@
+import random
+
 
 class Deck:
-    pass
+    def __init__(self):
+        self.cards = []
+        self.build()
 
+    def build(self):
+        """create all possible card combinations"""
+        for suit in ["Hearts", "Diamonds", "Clubs", "Spades"]:
+            for rank in range(1, 14):
+                self.cards.append((suit, rank))
+
+    def shuffle(self):
+        """ shuffle the deck"""
+        random.shuffle(self.cards)
+
+    def deal_card(self):
+        """remove and return the last card"""
+        return self.cards.pop()
+
+
+deck = Deck()
+print(deck.cards)
 
