169aafdc07530f3dcf57d1062a3cc7713bbffbf7
Cezary Cezary <cezary.game@gmail.com>
2023-04-25 23:40:57 +0200

    added class Game, Human, Croupier, Player

--- Module_1/blackjack.py
+++ Module_1/blackjack.py
@@ -1,4 +1,48 @@
 import random
+from faker import Faker
+
+
+# po inicjalizacji deck trzeba bedzie aktualziwoac deck po rozdaniu kart
+class Game:
+    def __init__(self, croupier, players, deck):
+        self.croupier = croupier
+        self.players = players
+        self.deck = deck
+
+    def start_game(self):
+        self.deck.shuffle()
+        for player in self.players:
+            for _ in range(2):
+                card = self.deck.deal_card()
+                player.cards.append(card)
+
+
+
+class Human:
+    def __init__(self):
+        self.cards = []
+        self.name = ""
+
+
+class Player(Human):
+    def __init__(self):
+        super().__init__()
+        fake = Faker()
+        self.name = fake.name()
+
+    @classmethod
+    def create_random_players(cls, min_players=2, max_players=7):
+        num_players = random.randint(min_players, max_players)
+        players = []
+        for i in range(num_players):
+            player = cls()
+
+
+class Croupier(Human):
+    def __init__(self):
+        super().__init__()
+        fake = Faker()
+        self.name = fake.name()
 
 
 class Deck:
@@ -21,6 +65,8 @@ class Deck:
         return self.cards.pop()
 
 
+random_players = Player.create_random_players(min_players=2, max_players=7)
+game = Game(croupier=Croupier(), )
 deck = Deck()
-print(deck.cards)
+
 
