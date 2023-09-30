036fac4d4cc8f73b3509f47ff42361465c950b90
Cezary Cezary <cezary.game@gmail.com>
2023-09-30 23:24:29 +0200

    check given passwords if leaked

--- Module_2/project/project.py
+++ Module_2/project/project.py
@@ -1,34 +1,61 @@
 from logger import get_logger
 from requests import get
+from hashlib import sha1
+
+logger = get_logger()
 
 
-def load_leaked_passwords():
-    url = "https://haveibeenpwned.com/API/v3"
-
-    with get(url) as content:
-        for row in content.json():
-            print(row)
+def hex_password(password):
+    hashed_password = sha1(str(password).encode("utf-8"))
+    hexed_password = hashed_password.hexdigest()
+    return hexed_password.upper()
 
 
-def load_passwords(leaked_passwords):
-    path_to_file = "passwords.txt"
-    logger = get_logger()
+def prefix_hexed_password(hexed_password):
+    return hexed_password[0:5]
+
+
+def download_prefix_hashes(prefix):
+    url = f"https://api.pwnedpasswords.com/range/{prefix}"
+    response = get(url)
+    if response.status_code == 200:
+        content = response.text
+        cleaned_hashes = []
+
+        for line in content.split("\n"):
+            if line:
+                suffix = line.split(":")[0].strip().upper()
+                cleaned_hashes.append(prefix.upper() + suffix)
+        return cleaned_hashes
+
+    else:
+        print(f"Error with API: status code: {response.status_code}")
+
+
+def check_passwords():
 
     try:
-        with open(path_to_file, mode="r", encoding="UTF-8") as passwords, open("safe_passwords", mode="w") as safe_passwords:
+        with open("passwords.txt", mode="r", encoding="UTF-8") as passwords, \
+                open("safe_passwords", mode="w") as safe_passwords:
             for line in passwords:
                 password = line.strip()
-                if password in leaked_passwords:
-                    pass
+                hexed_password = hex_password(password)
+                print(hexed_password)
+                print(prefix_hexed_password(hexed_password))
+                leaked_passwords = download_prefix_hashes(prefix_hexed_password(hexed_password))
+                print(leaked_passwords)
+
+                if hexed_password in leaked_passwords:
+                    logger.warning(f"Leaked password {password}")
+                else:
+                    safe_passwords.write(password + "\n")
 
     except IOError:
         logger.error("Loading failed")
     else:
-        logger.info("Password loaded successfully")
-        return text_data
+        logger.info("Passwords checked successfully")
 
 
-leaked_passwords = load_leaked_passwords()
-print(leaked_passwords)
-# loaded_passwords = load_passwords(leaked_passwords)
-# print(loaded_passwords)
+if __name__ == "__main__":
+    check_passwords()
+
--- Module_2/project/temp.py
+++
@@ -1,9 +0,0 @@
-from requests import get
-from pprint import pprint
-url = "https://api.pwnedpasswords.com/range/00D4F"
-print(url)
-
-
-with get(url) as content:
-    for row in content.json():
-        pprint(row)
